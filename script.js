baee01db366cc4bb1a0de125afe2eb47b0143115
kiran-sree <kiranmanoharan2001@gmail.com>
2023-06-16 15:12:43 +0530

    deploy

--- script.js
+++ script.js
@@ -25,9 +25,16 @@ const members = [
 
 // Event data
 const events = [
-  { title: 'Spo', date: 'June 15, 2023' },
-  { title: 'Fit', date: 'July 2, 2023' },
-  { title: 'Gro', date: 'August 10, 2023' },
+  {date: 'YEAR, 2012 - ', title:'1st Anpoli for MAHADEVIKULANGARA AMMA("Sorry, there are no photos available for this event at the moment.")'},
+  {date: 'YEAR, 2013 - ', title:'2st Anpoli for MAHADEVIKULANGARA AMMA.'},
+  {date: 'YEAR, 2014 - ', title:'3rd Anpoli For MAHADEVIKULANGARA AMMA, Kochu Aswathi valiyakulangara.'},
+  {date: 'YEAR, 2015 - ', title:'19/01/2015 Morning 08.00am Malarvady Arts & Sports Club, Valiyakulangara Ammaye "Puthukundom" Karayilek Sweekarikkunnu, മലർവാടി ആർട്സ് & സ്പോർട്സ്  ക്ലബ്ബ് വലിയകുളങ്ങര അമ്മക്ക് മീന അശ്വതിക്ക് സമർപ്പിക്കുന്ന കുതിരയുടെ ഉളി കുത്തൽ ചടങ്ങ് ഇന്ന് രാവിലെ 07:50 നു നടന്നു, മലർവാടി കുതിരയുടെ കതിരുകാൽ നിവർത്തുന്നു, മലർവാടി കുതിരച്ചുവട്ടിൽ കഞ്ഞി സമർപ്പിക്കുന്നു, മലർവാടി കുതിര വലിയകുളങ്ങര അമ്മക്ക് സമർപ്പിക്കുന്നു.'},
+  {date: 'YEAR, 2016 - ', title:'4th Anpoli For MAHADEVIKULANGARA AMMA, 101 താലപൊലികളുടെ പ്രഭയിൽ അമ്മ ആടി എഴുന്നള്ളുന്നു.' },
+  {date: 'YEAR, 2017 - ', title:'പ്രവീൺ മെമ്മോറിയൽ കൗണ്ടി ക്രിക്കറ്റ് ടൂർണമെന്റ്' },
+  {date: 'YEAR, 2018 - ', title:'' },
+  {date: 'YEAR, 2019 - ', title:'വലിയകുളങ്ങര അമ്മ മലർവാടിയില്, 1st കൊച്ചു Kodugaloor Anpoli.'},
+  {date: 'YEAR, 2020 - ', title:' മലർവാടി ഈ വർഷം വലിയകുളങ്ങര  അമ്മക്ക് നൽകിയ പറ, 2nd കൊച്ചു Kodugaloor Anpoli.' },
+  {date: 'YEAR, 2023 - ', title:' Kochu Aswathi valiyakulangara.'}
 ];
 
 
@@ -62,6 +69,6 @@ members.forEach(member => {
 const eventList = document.querySelector('.event-list');
 events.forEach(event => {
   const listItem = document.createElement('li');
-  listItem.textContent = `${event.title} (${event.date})`;
+  listItem.textContent = `${event.date} ${event.title}`;
   eventList.appendChild(listItem);
 });
